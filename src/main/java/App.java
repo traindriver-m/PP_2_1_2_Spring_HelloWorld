53cbcff0b7288bb5f9e3c4c796637fef0697342c
Traindriver-M <sergioparovozov@mail.ru>
2022-04-11 02:12:40 +0300

    Практическая задача 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
